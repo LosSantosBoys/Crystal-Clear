0bf183e7e406c47d01e212086a8c9c165ac221ef
Samuel Marques <samuel.s.marques@protonmail.com>
2024-06-04 22:19:18 -0300

    add dropdown and ranks

--- lib/features/home/pages/leaderboard_page.dart
+++ lib/features/home/pages/leaderboard_page.dart
@@ -1,5 +1,7 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:project/core/widgets/section_widget.dart';
+import 'package:project/features/home/widgets/rank_listtile.dart';
 
 class LeaderboardPage extends StatefulWidget {
   const LeaderboardPage({super.key});
@@ -21,25 +23,90 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
           children: [
             Section(
               headerTitle: "Placar de Líderes",
-              headerTrailing: DropdownButton(
-                items: [
-                  DropdownMenuItem(
-                    child: Text("Diário"),
-                    value: "daily",
+              headerTrailing: DropdownButtonHideUnderline(
+                child: DropdownButton2(
+                  enableFeedback: true,
+                  value: "daily",
+                  onChanged: (value) {},
+                  buttonStyleData: ButtonStyleData(
+                    elevation: 0,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFFAFAFA),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
                   ),
-                  DropdownMenuItem(
-                    child: Text("Semanal"),
-                    value: "weekly",
+                  dropdownStyleData: const DropdownStyleData(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFAFAFA),
+                    ),
+                    elevation: 0,
                   ),
-                  DropdownMenuItem(
-                    child: Text("Mensal"),
-                    value: "monthly",
-                  ),
-                ],
-                onChanged: (value) {},
+                  items: const [
+                    DropdownMenuItem(
+                      value: "daily",
+                      child: Text("Diário"),
+                    ),
+                    DropdownMenuItem(
+                      value: "weekly",
+                      child: Text("Semanal"),
+                    ),
+                    DropdownMenuItem(
+                      value: "monthly",
+                      child: Text("Mensal"),
+                    ),
+                  ],
+                ),
               ),
               child: ListView(
                 shrinkWrap: true,
+                padding: EdgeInsets.zero,
+                children: const [
+                  RankListTile(
+                    name: "Nome Sobrenome",
+                    points: 1000,
+                    position: 1,
+                  ),
+                  RankListTile(
+                    name: "Nome Sobrenome",
+                    points: 900,
+                    position: 2,
+                  ),
+                  RankListTile(
+                    name: "Nome Sobrenome",
+                    points: 800,
+                    position: 3,
+                  ),
+                  RankListTile(
+                    name: "Nome Sobrenome",
+                    points: 700,
+                    position: 4,
+                  ),
+                  RankListTile(
+                    name: "Nome Sobrenome",
+                    points: 650,
+                    position: 5,
+                  ),
+                  RankListTile(
+                    name: "Nome Sobrenome",
+                    points: 550,
+                    position: 6,
+                  ),
+                  RankListTile(
+                    name: "Nome Sobrenome",
+                    points: 500,
+                    position: 7,
+                  ),
+                  RankListTile(
+                    name: "Você",
+                    points: 400,
+                    position: 8,
+                  ),
+                  RankListTile(
+                    name: "Nome Sobrenome",
+                    points: 300,
+                    position: 9,
+                  ),
+                ],
               ),
             ),
           ],
