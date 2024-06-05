23977fa46bd7fcf7e219691a5d87a39bc34fd583
Samuel Marques <samuel.s.marques@protonmail.com>
2024-06-04 21:35:50 -0300

    Create map_page.dart

---
+++ lib/features/home/pages/map_page.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+  static const CameraPosition initialCameraPosition = CameraPosition(
+    target: LatLng(-23.5505, -46.6333),
+    zoom: 11,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: initialCameraPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
+  }
+}
