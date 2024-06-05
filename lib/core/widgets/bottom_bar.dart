e54bd813cc48a7619cf1a33e3a698c133cff2f9b
Samuel Marques <samuel.s.marques@protonmail.com>
2024-06-04 21:22:52 -0300

    Create bottom_bar.dart

---
+++ lib/core/widgets/bottom_bar.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.white,
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            height: 73,
+            width: 54,
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {},
+                child: const Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.home),
+                    Text("Home"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 73,
+            width: 80,
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {},
+                child: const Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.fitness_center),
+                    Text("Desafios"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 40),
+          SizedBox(
+            height: 73,
+            width: 80,
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {},
+                child: const Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.forum),
+                    Text("Feed"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 73,
+            width: 54,
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {},
+                child: const Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.menu),
+                    Text("Mais"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
