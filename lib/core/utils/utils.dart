a1ac91641d63937d49a2e08280bcc8bddbe3fa1f
Samuel Marques <samuel.s.marques@protonmail.com>
2024-06-05 14:36:34 -0300

    add snackbar extension

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 extension StringExtension on String {
   String capitalize() {
     return "${this[0].toUpperCase()}${substring(1)}";
@@ -7,5 +9,75 @@ extension StringExtension on String {
 extension UserExtension on String {
   String toUsername() {
     return split('@').first;
+  }
+}
+
+extension SnackbarExtension on BuildContext {
+  void showErrorSnackbar(String message) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Row(
+          children: [
+            const Icon(
+              Icons.error_outline,
+              color: Colors.red,
+            ),
+            const SizedBox(width: 10),
+            Text(message),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showSuccessSnackbar(String message) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Row(
+          children: [
+            const Icon(
+              Icons.check_circle_outline,
+              color: Colors.green,
+            ),
+            const SizedBox(width: 10),
+            Text(message),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showInfoSnackbar(String message) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Row(
+          children: [
+            const Icon(
+              Icons.info_outline,
+              color: Colors.blue,
+            ),
+            const SizedBox(width: 10),
+            Text(message),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showWarningSnackbar(String message) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Row(
+          children: [
+            const Icon(
+              Icons.warning_amber_outlined,
+              color: Colors.orange,
+            ),
+            const SizedBox(width: 10),
+            Text(message),
+          ],
+        ),
+      ),
+    );
   }
 }
