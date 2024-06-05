60f72bc82b90d95ba6fd0aa5ba9ad130e89d399e
Samuel Marques <samuel.s.marques@protonmail.com>
2024-06-04 21:16:48 -0300

    replace staggeredgrid with collections widget

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:project/core/widgets/collection_grid.dart';
 import 'package:project/core/widgets/section_widget.dart';
 
 class HomePage extends StatefulWidget {
@@ -135,193 +135,27 @@ class _HomePageState extends State<HomePage> {
             Section(
               headerTitle: "Suas coletas",
               headerTrailing: TextButton(
-                // todo: redirect to collection page
-                onPressed: () {},
+                onPressed: () => Navigator.pushNamed(context, '/collections'),
                 child: const Text("ver mais"),
               ),
-              child: StaggeredGrid.count(
-                crossAxisCount: 4,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-                children: [
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 2,
-                    mainAxisCellCount: 2.4,
-                    child: Container(
-                      padding: const EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey[300]!),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: const Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.local_drink_outlined,
-                                size: 28,
-                                color: Color(0xff699BF7),
-                              ),
-                              SizedBox(width: 10),
-                              Text(
-                                "Plástico",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Spacer(),
-                          // todo: change the number of plastic collections
-                          Text(
-                            "12",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 40,
-                            ),
-                          ),
-                          Text(
-                            "vezes",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w300,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Spacer(),
-                        ],
-                      ),
-                    ),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 2,
-                    mainAxisCellCount: 1.2,
-                    child: Container(
-                      padding: const EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey[300]!),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.iron,
-                                size: 24,
-                                color: Color(0xff545454),
-                              ),
-                              SizedBox(width: 10),
-                              Text(
-                                "Metal",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
-                          ),
-                          const Spacer(),
-                          // todo: change the number of metal collections
-                          RichText(
-                            text: const TextSpan(
-                              text: "6",
-                              children: [
-                                TextSpan(
-                                  text: " vezes",
-                                  style: TextStyle(fontWeight: FontWeight.w300, fontSize: 14),
-                                ),
-                              ],
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                          const Spacer(),
-                        ],
-                      ),
-                    ),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 2,
-                    mainAxisCellCount: 1.2,
-                    child: Container(
-                      padding: const EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey[300]!),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.local_bar_outlined,
-                                size: 24,
-                                color: Color(0xffFF9A62),
-                              ),
-                              SizedBox(width: 10),
-                              Text(
-                                "Vidro",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
-                          ),
-                          const Spacer(),
-                          // todo: change the number of glass collections
-                          RichText(
-                            text: const TextSpan(
-                              text: "4",
-                              children: [
-                                TextSpan(
-                                  text: " vezes",
-                                  style: TextStyle(fontWeight: FontWeight.w300, fontSize: 14),
-                                ),
-                              ],
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                          const Spacer(),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              child: const CollectionGrid(),
             ),
             Section(
               headerTitle: "Encontre entulhos próximos",
               child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Container(
-                    width: double.infinity,
-                    height: 312,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        // todo: change to working map
-                        image: NetworkImage("https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/02/google-maps-e1707316052388.png"),
-                        fit: BoxFit.cover,
-                      ),
+                borderRadius: BorderRadius.circular(12),
+                child: Container(
+                  width: double.infinity,
+                  height: 312,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      // todo: change to working map
+                      image: NetworkImage("https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/02/google-maps-e1707316052388.png"),
+                      fit: BoxFit.cover,
                     ),
-                  )),
+                  ),
+                ),
+              ),
             ),
             Section(
               headerTitle: "Placar de Líderes",
