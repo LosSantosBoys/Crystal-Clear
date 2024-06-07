bb827aff63df14fa96fd72bd05a03e5f83e9da90
Samuel Marques <samuel.s.marques@protonmail.com>
2024-06-06 21:30:21 -0300

    changes page's index

--- lib/core/widgets/bottom_bar.dart
+++ lib/core/widgets/bottom_bar.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 
-class BottomBar extends StatelessWidget {
-  const BottomBar({super.key, required this.scaffoldKey});
+class BottomBar extends StatefulWidget {
+  const BottomBar({
+    super.key,
+    required this.scaffoldKey,
+    required this.pageController,
+  });
 
   final GlobalKey<ScaffoldState> scaffoldKey;
+  final PageController pageController;
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.pageController.addListener(() {
+      setState(() {
+        index = widget.pageController.page!.round();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,13 +41,27 @@ class BottomBar extends StatelessWidget {
             child: Material(
               color: Colors.transparent,
               child: InkWell(
-                onTap: () {},
-                child: const Column(
+                onTap: () {
+                  widget.pageController.animateToPage(
+                    0,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                child: Column(
                   mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.home),
-                    Text("Home"),
+                    Icon(
+                      Icons.home,
+                      color: index == 0 ? const Color(0xFF386BF6) : const Color(0xFF9DB2CE),
+                    ),
+                    Text(
+                      "Home",
+                      style: TextStyle(
+                        color: index == 0 ? const Color(0xFF386BF6) : const Color(0xFF9DB2CE),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -37,13 +73,27 @@ class BottomBar extends StatelessWidget {
             child: Material(
               color: Colors.transparent,
               child: InkWell(
-                onTap: () {},
-                child: const Column(
+                onTap: () {
+                  widget.pageController.animateToPage(
+                    1,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                child: Column(
                   mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.fitness_center),
-                    Text("Desafios"),
+                    Icon(
+                      Icons.fitness_center,
+                      color: index == 1 ? const Color(0xFF386BF6) : const Color(0xFF9DB2CE),
+                    ),
+                    Text(
+                      "Conquistas",
+                      style: TextStyle(
+                        color: index == 1 ? const Color(0xFF386BF6) : const Color(0xFF9DB2CE),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -57,12 +107,20 @@ class BottomBar extends StatelessWidget {
               color: Colors.transparent,
               child: InkWell(
                 onTap: () {},
-                child: const Column(
+                child: Column(
                   mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.forum),
-                    Text("Feed"),
+                    Icon(
+                      Icons.forum,
+                      color: index == 2 ? const Color(0xFF386BF6) : const Color(0xFF9DB2CE),
+                    ),
+                    Text(
+                      "Feed",
+                      style: TextStyle(
+                        color: index == 2 ? const Color(0xFF386BF6) : const Color(0xFF9DB2CE),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -74,13 +132,21 @@ class BottomBar extends StatelessWidget {
             child: Material(
               color: Colors.transparent,
               child: InkWell(
-                onTap: () => scaffoldKey.currentState!.openEndDrawer(),
-                child: const Column(
+                onTap: () => widget.scaffoldKey.currentState!.openEndDrawer(),
+                child: Column(
                   mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.menu),
-                    Text("Mais"),
+                    Icon(
+                      Icons.menu,
+                      color: index == 3 ? const Color(0xFF386BF6) : const Color(0xFF9DB2CE),
+                    ),
+                    Text(
+                      "Mais",
+                      style: TextStyle(
+                        color: index == 3 ? const Color(0xFF386BF6) : const Color(0xFF9DB2CE),
+                      ),
+                    ),
                   ],
                 ),
               ),
