c9c26104bc8340f5afa015ff03e012adc089a7de
Samuel Marques <samuel.s.marques@protonmail.com>
2024-06-06 23:21:18 -0300

    add achievement generation for mockup

--- lib/features/achievements/achievement_page.dart
+++ lib/features/achievements/achievement_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:crystalclear/features/achievements/enum/achievement_type.dart';
+import 'package:crystalclear/features/achievements/models/achievement.dart';
 import 'package:crystalclear/features/achievements/widgets/achievement_listtile.dart';
 import 'package:flutter/material.dart';
 
@@ -10,19 +13,79 @@ class AchievementPage extends StatefulWidget {
 }
 
 class _AchievementPageState extends State<AchievementPage> {
+  List<Achievement> achievements = [];
+
+  void generateRandomAchievements() {
+    // Generate random achievements
+    for (int i = 0; i < 6; i++) {
+      AchievementType type = AchievementType.values[Random().nextInt(AchievementType.values.length)];
+      String title = generateRandomTitle();
+      String subtitle = generateRandomSubtitle();
+      bool achieved = Random().nextBool();
+
+      Achievement achievement = Achievement(
+        title: title,
+        subtitle: subtitle,
+        type: type,
+        achieved: achieved,
+      );
+
+      achievements.add(achievement);
+    }
+
+    achievements.sort((a, b) => a.achieved ? -1 : 1);
+  }
+
+  String generateRandomTitle() {
+    List<String> titles = [
+      "Praieiro",
+      "Aprendiz de Limpeza",
+      "Tudo no seu devido lugar",
+      "Procurando Nemo",
+      "Coletor",
+      "Aventureiro",
+      "Lenda",
+      "Herói",
+    ];
+
+    return titles[Random().nextInt(titles.length)];
+  }
+
+  String generateRandomSubtitle() {
+    List<String> subtitles = [
+      "Colete entulho de 5 praias diferentes.",
+      "Descubra todos os segredos do mapa.",
+      "Vença 10 partidas consecutivas.",
+      "Alcance o nível máximo em todas as habilidades.",
+      "Colete todos os itens raros.",
+      "Explore todas as regiões do Brasil.",
+      "Se torne uma lenda viva.",
+      "Salve o mundo de uma grande ameaça.",
+    ];
+
+    return subtitles[Random().nextInt(subtitles.length)];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    generateRandomAchievements();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: ListView.builder(
-        itemCount: 1,
+        itemCount: achievements.length,
         shrinkWrap: true,
         itemBuilder: (BuildContext context, int index) {
-          return const AchievementListTile(
-            title: "Praieiro",
-            subtitle: "Colete entulho de 5 praias diferentes.",
-            type: AchievementType.epic,
-            achieved: true,
+          return AchievementListTile(
+            title: achievements.elementAt(index).title,
+            subtitle: achievements.elementAt(index).subtitle,
+            type: achievements.elementAt(index).type,
+            achieved: achievements.elementAt(index).achieved,
           );
         },
       ),
